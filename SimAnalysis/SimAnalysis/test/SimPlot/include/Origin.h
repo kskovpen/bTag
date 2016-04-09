4f3355ff90ab22a5d0951e86517e26a83d99a07b
kskovpen <Kirill.Skovpen@cern.ch>
2016-04-09 21:14:45 +0200

    add reco + more MC truth info

--- SimAnalysis/SimAnalysis/test/SimPlot/include/Origin.h
+++ SimAnalysis/SimAnalysis/test/SimPlot/include/Origin.h
@@ -22,6 +22,49 @@ namespace SIMPLOT
 	int partonIdLabel(int partonId);
 	
 	float DeltaR(float eta1,float phi1,float eta2,float phi2);
+
+	struct GenSim
+	  {   
+	     int idx;
+	     bool type;
+	     float pt;
+	     float eta;
+	     float phi;
+	     int pdgId;
+	     int idxReco;
+	     float drReco;
+	  };
+
+	static bool sortByPt(const GenSim &a, const GenSim &b)
+	  {	     
+	     return a.pt > b.pt;
+	  };	
+
+	bool BTVMuonSelection(int numberOfValidMuonHits,
+			      int numberOfMatches,
+			      int numberOfMatchedStations,
+			      int trackerLayersWithMeasurement,
+			      int numberOfValidHits,
+			      int numberOfValidPixelHits,
+			      int numberOfHits,
+			      float normalizedChi2GlobalTrack,
+			      float normalizedChi2InnerTrack,
+			      bool isPFMuon,
+			      bool isGlobalMuon
+			     );
+
+	bool NewMuonSelection(int numberOfValidMuonHits,
+			      int numberOfMatches,
+			      int numberOfMatchedStations,
+			      int trackerLayersWithMeasurement,
+			      int numberOfValidHits,
+			      int numberOfValidPixelHits,
+			      int numberOfHits,
+			      float normalizedChi2GlobalTrack,
+			      float normalizedChi2InnerTrack,
+			      bool isPFMuon,
+			      bool isGlobalMuon
+			     );
 	
       private:
 	
@@ -38,6 +81,9 @@ namespace SIMPLOT
 	
 	TH1F *h_mu1_partonId;
 	TH1F *h_mu2_partonId;
+
+	TH1F *h_muRec1_id;
+	TH1F *h_muRec2_id;
 	
 	TH1F *h_mu1_pt;
 	TH1F *h_mu2_pt;
@@ -59,6 +105,47 @@ namespace SIMPLOT
 	TH1F *h_muRec1_pt;
 	TH1F *h_muRec2_pt;
 
+	TH1F *h_muSimMatchedRec1_pt;
+	TH1F *h_muSimMatchedRec2_pt;
+
+	TH1F *h_muGenMatchedRec1_pt;
+	TH1F *h_muGenMatchedRec2_pt;
+
+	TH1F *h_muAllMatchedRec1_pt;
+	TH1F *h_muAllMatchedRec2_pt;
+
+	TH1F *h_muAllBD1_pt;
+	TH1F *h_muAllBD2_pt;
+	
+	TH1F *h_muAllOTHER1_pt;
+	TH1F *h_muGenOTHER1_pt;
+	TH1F *h_muSimOTHER1_pt;
+
+	TH1F *h_muAllOTHER2_pt;
+	TH1F *h_muGenOTHER2_pt;
+	TH1F *h_muSimOTHER2_pt;
+	
+	TH1F *h_muAllPK1_pt;
+	TH1F *h_muAllPK2_pt;	
+	
+	TH1F *h_muAllBDMatchedRec1_pt;
+	TH1F *h_muAllBDMatchedRec2_pt;
+	
+	TH1F *h_muAllPKMatchedRec1_pt;
+	TH1F *h_muAllPKMatchedRec2_pt;
+	
+	TH1F *h_muRecClosestMuGen1_dr;
+	TH1F *h_muRecClosestMuGen2_dr;
+
+	TH1F *h_muRecClosestMuSim1_dr;
+	TH1F *h_muRecClosestMuSim2_dr;
+
+	TH1F *h_muGenClosestMuRec1_dr;
+	TH1F *h_muGenClosestMuRec2_dr;
+
+	TH1F *h_muSimClosestMuRec1_dr;
+	TH1F *h_muSimClosestMuRec2_dr;
+	
 	TH1F *h_muJet1_pt;
 	TH1F *h_muJet2_pt;	
 
@@ -69,7 +156,58 @@ namespace SIMPLOT
 	TH1F *h_muCJet2_pt;	
 
 	TH1F *h_muLJet1_pt;
-	TH1F *h_muLJet2_pt;	
+	TH1F *h_muLJet2_pt;
+	
+	TH1F *h_muPK1_gensim;
+	TH1F *h_muPK2_gensim;
+	TH1F *h_muPK21_gensim;
+	
+	TH1F *h_muOTHER1_gensim;
+	TH1F *h_muOTHER2_gensim;
+	TH1F *h_muNOTPK2_gensim;
+	TH1F *h_muOTHER21_gensim;
+
+	TH1F *h_muFake_pt;
+	TH1F *h_muReal_pt;
+	
+	TH1F *h_muVal1_pt;
+	TH1F *h_muVal2_pt;
+	TH1F *h_muVal3_pt;
+
+	TH1F *h_muValUncor1_pt;
+	TH1F *h_muValUncor2_pt;
+	TH1F *h_muValUncor3_pt;
+	
+	TH1F *h_muRecVal1_pt;
+	TH1F *h_muRecVal2_pt;
+	TH1F *h_muRecVal3_pt;
+
+	TH1F *h_muRecValUncor1_pt;
+	TH1F *h_muRecValUncor2_pt;
+	TH1F *h_muRecValUncor3_pt;
+	
+	TH1F *h_muFake_trackerLayersWithMeasurement;
+	TH1F *h_muFake_numberOfValidMuonHits;
+	TH1F *h_muFake_numberOfMatches;
+	TH1F *h_muFake_numberOfMatchedStations;
+	TH1F *h_muFake_numberOfValidHits;
+	TH1F *h_muFake_numberOfValidPixelHits;
+	TH1F *h_muFake_numberOfHits;
+	TH1F *h_muFake_normalizedChi2GlobalTrack;
+	TH1F *h_muFake_normalizedChi2InnerTrack;
+	
+	TH1F *h_muReal_trackerLayersWithMeasurement;
+	TH1F *h_muReal_numberOfValidMuonHits;
+	TH1F *h_muReal_numberOfMatches;
+	TH1F *h_muReal_numberOfMatchedStations;
+	TH1F *h_muReal_numberOfValidHits;
+	TH1F *h_muReal_numberOfValidPixelHits;
+	TH1F *h_muReal_numberOfHits;
+	TH1F *h_muReal_normalizedChi2GlobalTrack;
+	TH1F *h_muReal_normalizedChi2InnerTrack;
+	
+	TH1F *h_muPythiaGeant_N;
+	TH1F *h_muGeant_N;
      };
 }
 
--- SimAnalysis/SimAnalysis/test/SimPlot/include/Tree.h
+++ SimAnalysis/SimAnalysis/test/SimPlot/include/Tree.h
@@ -40,11 +40,41 @@ public :
    std::vector<float>         *muSimEta;
    std::vector<float>         *muSimPhi;
    std::vector<int>           *muSimMomId;
-   
+
    std::vector<float>         *recomuonPt;
    std::vector<float>         *recomuonEta;
    std::vector<float>         *recomuonPhi;
 
+   std::vector<bool>          *recomuonIsPFMuon;
+   std::vector<bool>          *recomuonIsGlobalMuon;
+   std::vector<int>           *recomuonNumberOfValidMuonHits;
+   std::vector<int>           *recomuonNumberOfMatches;
+   std::vector<int>           *recomuonNumberOfValidHits;
+   std::vector<int>           *recomuonNumberOfValidPixelHits;
+   std::vector<int>           *recomuonNumberOfHits;
+   std::vector<float>         *recomuonNormalizedChi2GlobalTrack;
+   std::vector<float>         *recomuonNormalizedChi2InnerTrack;
+   std::vector<int>           *recomuonNumberOfValidMuonHitsGlobalTrack;
+   std::vector<int>           *recomuonTrackerLayersWithMeasurement;
+   std::vector<int>           *recomuonNumberOfMatchedStations;
+   
+   std::vector<float>         *patmuonPt;
+   std::vector<float>         *patmuonEta;
+   std::vector<float>         *patmuonPhi;
+
+   std::vector<bool>          *patmuonIsPFMuon;
+   std::vector<bool>          *patmuonIsGlobalMuon;
+   std::vector<int>           *patmuonNumberOfValidMuonHits;
+   std::vector<int>           *patmuonNumberOfMatches;
+   std::vector<int>           *patmuonNumberOfValidHits;
+   std::vector<int>           *patmuonNumberOfValidPixelHits;
+   std::vector<int>           *patmuonNumberOfHits;
+   std::vector<float>         *patmuonNormalizedChi2GlobalTrack;
+   std::vector<float>         *patmuonNormalizedChi2InnerTrack;
+   std::vector<int>           *patmuonNumberOfValidMuonHitsGlobalTrack;
+   std::vector<int>           *patmuonTrackerLayersWithMeasurement;
+   std::vector<int>           *patmuonNumberOfMatchedStations;
+   
    std::vector<float>         *patjetPt;
    std::vector<float>         *patjetEta;
    std::vector<float>         *patjetPhi;
@@ -73,11 +103,41 @@ public :
    TBranch        *b_muSimEta;
    TBranch        *b_muSimPhi;
    TBranch        *b_muSimMomId;
-   
+
    TBranch        *b_recomuonPt;
    TBranch        *b_recomuonEta;
    TBranch        *b_recomuonPhi;
 
+   TBranch        *b_recomuonIsPFMuon;
+   TBranch        *b_recomuonIsGlobalMuon;
+   TBranch        *b_recomuonNumberOfValidMuonHits;
+   TBranch        *b_recomuonNumberOfMatches;
+   TBranch        *b_recomuonNumberOfValidHits;
+   TBranch        *b_recomuonNumberOfValidPixelHits;
+   TBranch        *b_recomuonNumberOfHits;
+   TBranch        *b_recomuonNormalizedChi2GlobalTrack;
+   TBranch        *b_recomuonNormalizedChi2InnerTrack;
+   TBranch        *b_recomuonNumberOfValidMuonHitsGlobalTrack;
+   TBranch        *b_recomuonTrackerLayersWithMeasurement;
+   TBranch        *b_recomuonNumberOfMatchedStations;
+   
+   TBranch        *b_patmuonPt;
+   TBranch        *b_patmuonEta;
+   TBranch        *b_patmuonPhi;
+
+   TBranch        *b_patmuonIsPFMuon;
+   TBranch        *b_patmuonIsGlobalMuon;
+   TBranch        *b_patmuonNumberOfValidMuonHits;
+   TBranch        *b_patmuonNumberOfMatches;
+   TBranch        *b_patmuonNumberOfValidHits;
+   TBranch        *b_patmuonNumberOfValidPixelHits;
+   TBranch        *b_patmuonNumberOfHits;
+   TBranch        *b_patmuonNormalizedChi2GlobalTrack;
+   TBranch        *b_patmuonNormalizedChi2InnerTrack;
+   TBranch        *b_patmuonNumberOfValidMuonHitsGlobalTrack;
+   TBranch        *b_patmuonTrackerLayersWithMeasurement;
+   TBranch        *b_patmuonNumberOfMatchedStations;
+   
    TBranch        *b_patjetPt;
    TBranch        *b_patjetEta;
    TBranch        *b_patjetPhi;
